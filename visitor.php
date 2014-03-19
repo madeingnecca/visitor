4a06b358cf2c647c1cc4935576d34f774c0edac5
Damiano Seno <dax86it@gmail.com>
2014-03-19 02:24:24 +0100

    Added authentication support

--- visitor.php
+++ visitor.php
@@ -7,10 +7,20 @@ function http_request($url, $options = array()) {
     die('CURL library must be installed to download files.');
   }
 
+  $options += array('auth' => FALSE);
+
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_HEADER, 0);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+
+  if ($options['auth']) {
+    curl_setopt($ch, CURLOPT_USERPWD, $options['auth']);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  }
+
   $data = curl_exec($ch);
   $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
   curl_close($ch);
@@ -31,11 +41,13 @@ function collect_urls($page_html, $page_url, $options = array()) {
 
   $url_parsed = parse_url($page_url);
   $url_parsed += array('scheme' => 'http', 'path' => '');
+  $url_base = dirname($url_parsed['path']);
+
   $domain = $url_parsed['scheme'] . '://' . $url_parsed['host'];
 
   $result = array();
   foreach ($options['tags'] as $tag => $attrs) {
-    if (preg_match_all('$<' . $tag . '[^>]+(?:' . join('|', $attrs) . ')="([^#].*?)"$msi', $page_html, $matches)) {
+    if (preg_match_all('%<' . $tag . '[^>]+(?:' . join('|', $attrs) . ')="([^#].*?)"%', $page_html, $matches)) {
       foreach ($matches[1] as $match) {
         // Handle protocol-relative urls.
         if (substr($match, 0, 2) == '//') {
@@ -50,10 +62,12 @@ function collect_urls($page_html, $page_url, $options = array()) {
         $match_parsed = parse_url($match);
         $match_parsed += array('path' => '');
 
+        // Other kind of relative urls.
+        // @TODO: implement directory traversal.
         if (!isset($match_parsed['scheme']) && !isset($match_parsed['host'])) {
           $match_parsed['scheme'] = $url_parsed['scheme'];
           $match_parsed['host'] = $url_parsed['host'];
-          $match_parsed['path'] = '/' . $match_parsed['path'];
+          $match_parsed['path'] = $url_base . '/' . $match_parsed['path'];
         }
 
         if (!in_array($match_parsed['scheme'], $options['protocols'])) {
@@ -62,16 +76,12 @@ function collect_urls($page_html, $page_url, $options = array()) {
 
         $match_domain = $match_parsed['scheme'] . '://' . $match_parsed['host'];
 
-        if ($options['stay_in_domain'] && $match_domain != $domain) {
-          continue;
-        }
-
         $match_assembled = $match_parsed['scheme'] . '://' . $match_parsed['host'] . $match_parsed['path'];
         if (isset($match_parsed['query'])) {
           $match_assembled .= '?' . $match_parsed['query'];
         }
 
-        $result[] = $match_assembled;
+        $result[] = array('url' => $match_assembled, 'collect' => ($options['stay_in_domain'] && $match_domain == $domain));
       }
     }
   }
@@ -99,8 +109,14 @@ if ($argc == 1) {
 
 $options = getopt('u:f:');
 $format = '%url';
+$auth = FALSE;
+
 if (isset($options['f']) && $options['f']) {
   $format = $options['f'];
+}
+
+if (isset($options['u']) && $options['u']) {
+  $auth = $options['u'];
 }
 
 $start = $argv[$argc - 1];
@@ -117,14 +133,14 @@ $queue[] = array('url' => $start);
 
 while (!empty($queue)) {
   $url_data = array_pop($queue);
-  $url_data += array('parents' => array());
+  $url_data += array('parents' => array(), 'collect' => TRUE);
   $url = $url_data['url'];
 
   if (isset($visited[$url])) {
     continue;
   }
 
-  $response = http_request($url);
+  $response = http_request($url, array('auth' => $auth));
 
   $visit = array();
   $visit['url'] = $url;
@@ -136,11 +152,13 @@ while (!empty($queue)) {
   print format_url($format, $url, $visit);
   print PHP_EOL;
 
-  if ($response['code'] == 200) {
+  if ($url_data['collect'] && $response['code'] == 200) {
     $urls = collect_urls($response['data'], $url);
 
+    $new_parents = array_merge($url_data['parents'], array($url));
     foreach ($urls as $collected) {
-      $queue[] = array('url' => $collected, 'parents' => array_merge($url_data['parents'], array($url)));
+      $collected += array('parents' => $new_parents);
+      $queue[] = $collected;
     }
   }
 }
