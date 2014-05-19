bbdb9f538691f97689c995bbe37701dd3a6dea4b
madeingnecca <dax86it@gmail.com>
2014-05-20 01:18:08 +0200

    added presets + refactored url collection via xpath + added %referrer in format string

--- visitor.php
+++ visitor.php
@@ -17,8 +17,11 @@ function http_request($url, $options = array()) {
   $options += array(
     'auth' => FALSE,
     'user-agent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',
-    'max-redirects' => 100,
+    'max-redirects' => 15,
+    'method' => 'GET',
   );
+
+  $method = strtoupper($options['method']);
 
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_HEADER, TRUE);
@@ -32,6 +35,10 @@ function http_request($url, $options = array()) {
   if ($options['auth']) {
     curl_setopt($ch, CURLOPT_USERPWD, $options['auth']);
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  }
+
+  if ($method != 'GET' && $method != 'POST') {
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
   }
 
   $data = curl_exec($ch);
@@ -92,41 +99,75 @@ function collect_urls($page_html, $page_url, $options = array()) {
 
   $domain = $url_parsed['scheme'] . '://' . $url_parsed['host'];
 
+  // List of collected urls.
   $result = array();
+
+  // Parse the html document.
+  $dom = new DOMDocument();
+  libxml_use_internal_errors(TRUE);
+
+  $dom_loaded = $dom->loadHTML($page_html);
+
+  if ($dom_loaded === FALSE) {
+    return array();
+  }
+
+  $xpath = new DOMXpath($dom);
+
+  // Traverse the document via xpath.
   foreach ($options['tags'] as $tag => $attrs) {
-    if (preg_match_all('%<' . $tag . '[^>]+(?:' . join('|', $attrs) . ')="([^#].*?)"%', $page_html, $matches)) {
-      foreach ($matches[1] as $match) {
-        // Handle protocol-relative urls.
-        if (substr($match, 0, 2) == '//') {
-          $match = $url_parsed['scheme'] . ':' . $match;
-        }
+    $nodes = $xpath->query('//' . $tag);
 
-        // Handle root-relative.
-        if ($match[0] == '/') {
-          $match = $domain . $match;
-        }
+    foreach ($nodes as $node) {
+      foreach ($attrs as $attr) {
+        $value = $node->getAttribute($attr);
+        $orig_value = $value;
 
-        $match_parsed = parse_url($match);
-        $match_parsed += array('path' => '');
-
-        // Other kind of relative urls.
-        if (!isset($match_parsed['scheme']) && !isset($match_parsed['host'])) {
-          $match_parsed['scheme'] = $url_parsed['scheme'];
-          $match_parsed['host'] = $url_parsed['host'];
-          $match_parsed['path'] = resolve_relative_url($url_base, $match_parsed['path']);
-        }
-
-        if (!in_array($match_parsed['scheme'], $options['protocols'])) {
+        if (empty($value)) {
           continue;
         }
 
-        $match_domain = $match_parsed['scheme'] . '://' . $match_parsed['host'];
-        $match_assembled = assemble_url($match_parsed);
+        if ($value[0] == '#') {
+          continue;
+        }
 
-        $result[] = array('url' => $match_assembled, 'collect' => ($options['stay_in_domain'] && $match_domain == $domain));
+        // Handle protocol-relative urls.
+        if (substr($value, 0, 2) == '//') {
+          $value = $url_parsed['scheme'] . ':' . $value;
+        }
+
+        // Handle root-relative.
+        if ($value[0] == '/') {
+          $value = $domain . $value;
+        }
+
+        $value_parsed = parse_url($value);
+        $value_parsed += array('path' => '');
+
+        // Other kind of relative urls.
+        if (!isset($value_parsed['scheme']) && !isset($value_parsed['host'])) {
+          $value_parsed['scheme'] = $url_parsed['scheme'];
+          $value_parsed['host'] = $url_parsed['host'];
+          $value_parsed['path'] = resolve_relative_url($url_base, $value_parsed['path']);
+        }
+
+        if (!in_array($value_parsed['scheme'], $options['protocols'])) {
+          continue;
+        }
+
+        $value_domain = $value_parsed['scheme'] . '://' . $value_parsed['host'];
+
+        $value_assembled = assemble_url($value_parsed);
+
+        $result[] = array('url' => $value_assembled, 'collect' => ($options['stay_in_domain'] && $value_domain == $domain));
       }
     }
   }
+
+  // Prevent DOMDocument log memory leak.
+  // http://stackoverflow.com/questions/8379829/domdocument-php-memory-leak
+  unset($dom);
+  libxml_use_internal_errors(FALSE);
 
   return $result;
 }
@@ -148,7 +189,7 @@ function resolve_relative_url($base_path, $rel_path) {
 }
 
 function assemble_url($parsed) {
-  $assembled = $parsed['scheme'] . '://' . $parsed['host'] . $parsed['path'];
+  $assembled = $parsed['scheme'] . '://' . rtrim($parsed['host'], '/') . '/' . ltrim($parsed['path'], '/');
   if (isset($parsed['query'])) {
     $assembled .= '?' . str_replace('&amp;', '&', $parsed['query']);
   }
@@ -172,32 +213,58 @@ function format_url($format, $url, $data) {
   return $result;
 }
 
+function preset_list() {
+  $presets = array();
+  $presets['health'] = array(
+    'http' => array(),
+    'collect' => array(
+      'tags' => array(
+        '*' => array('href', 'src'),
+      ),
+    ),
+    'format' => '%url %code',
+  );
+
+  return $presets;
+}
+
 if (php_sapi_name() != 'cli') {
   die('No CLI, no party.');
 }
 
 $pcount = $argc - 1;
-$options = getopt('u:f:');
-$format = '%url';
-$auth = FALSE;
+$options = getopt('u:f:p');
+$params = array();
+
+$presets = preset_list();
+$preset_name = 'health';
 
 foreach ($options as $opt => $value) {
   $pcount --;
 
-  if ($value) {
+  if ($value !== FALSE) {
     $pcount --;
 
     switch ($opt) {
-      case 'f': $format = $value; break;
-      case 'u': $auth = $value; break;
+      case 'f': $params['format'] = $value; break;
+      case 'u': $params['auth'] = $value; break;
+      case 'p': $preset_name = $value; break;
     }
   }
 }
 
 if ($pcount != 1) {
   show_usage();
-  die();
+  exit;
 }
+
+if (!isset($presets[$preset_name])) {
+  print "Preset '$preset_name' was not found. Choose between: " . join(', ', array_keys($presets));
+  print "\n";
+  exit;
+}
+
+$params = array_merge($presets[$preset_name], $params);
 
 // Start url is always the last parameter.
 $start = $argv[$argc - 1];
@@ -213,23 +280,24 @@ $queue[] = array('url' => $start);
 
 while (!empty($queue)) {
   $url_data = array_pop($queue);
-  $url_data += array('parents' => array(), 'collect' => TRUE);
+  $url_data += array('parents' => array(), 'collect' => TRUE, 'referrer' => '');
   $url = $url_data['url'];
 
   if (isset($visited[$url])) {
     continue;
   }
 
-  $response = http_request($url, array('auth' => $auth));
+  $response = http_request($url, array_merge($params['http'], array('auth' => $params['format'])));
 
   $visit = array();
   $visit['url'] = $url;
   $visit['parents'] = join(' --> ', $url_data['parents']);
+  $visit['referrer'] = end($url_data['parents']);
   $visit += $response;
 
   $visited[$url] = $visit;
 
-  print format_url($format, $url, $visit);
+  print format_url($params['format'], $url, $visit);
   print "\n";
 
   $is_web_page = (strpos($response['content_type'], 'text/html') === 0);
@@ -237,7 +305,7 @@ while (!empty($queue)) {
   // Collect urls only if it was a successful response, a page containing html
   // and collection was requested.
   if ($response['code'] == 200 && $is_web_page && $url_data['collect']) {
-    $urls = collect_urls($response['data'], $url);
+    $urls = collect_urls($response['data'], $url, $params['collect']);
 
     $new_parents = array_merge($url_data['parents'], array($url));
     foreach ($urls as $collected) {
