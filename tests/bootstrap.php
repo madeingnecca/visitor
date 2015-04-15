0cbe7f6a427d7e6c36e688daaefc790c80fbde9c
madeingnecca <dax86it@gmail.com>
2015-04-16 00:35:29 +0200

    Fixes wrong results caused by timezone warning

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 
+// PHP warnings are annoying yes, but also make phpunit fail.
+date_default_timezone_set('UTC');
+
 // Require "Visitor" as a library.
 require_once __DIR__ . '/../visitor.php';
 
